9ba577f61489fb4019331f29172bc9e2309e7930
Nyanguk <lobgd9150@gmail.com>
2021-01-14 11:25:13 +0900

    호스트 이름 변경

--- mysite/settings.py
+++ mysite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'nut-!=ojy@q@t7$+5i)l4g+&)h-ax%2ope1@cc)wbe#a_6p3-('
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', '.pythonanywhere.com']
 
 
 # Application definition
