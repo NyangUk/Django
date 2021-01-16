bd5cb998f3e2c243ee39d01447439b2e8445e096
Nyanguk <lobgd9150@gmail.com>
2021-01-17 01:44:07 +0900

    front-end: 문자열 띄우기

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,5 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse
 # Create your views here.
+def post_list(request):
+    return HttpResponse("아직 구현하지 못했어! 다음 튜토리얼에서 보자!")
