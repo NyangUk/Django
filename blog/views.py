dd82d928a1cdd2f73bbf9177ea366444f7c037ed
Nyanguk <lobgd9150@gmail.com>
2021-01-17 19:43:48 +0900

    url 요청시 post_list.html로 연결되게함

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,4 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 # Create your views here.
 def post_list(request):
-    return HttpResponse("아직 구현하지 못했어! 다음 튜토리얼에서 보자!")
+    return render(request, 'blog/post_list.html', {})
