ceec1efa463799ab38dbccea7bd713f0d2be57bb
Nyanguk <lobgd9150@gmail.com>
2021-01-19 00:40:59 +0900

    Post의 쿼리셋을 템플릿으로 전달

--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render
+from .models import Post
 # Create your views here.
+posts=Post.objects.all()
 def post_list(request):
-    return render(request, 'blog/post_list.html', {})
+    return render(request, 'blog/post_list.html', {'posts'=posts})
