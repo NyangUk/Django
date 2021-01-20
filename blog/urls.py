ce87d6b05c1369119902d89f6f5afdcc7d845080
Nyanguk <lobgd9150@gmail.com>
2021-01-20 11:43:34 +0900

    게시글 상세페이지 구현

--- blog/urls.py
+++ blog/urls.py
@@ -1,5 +1,7 @@
+# blog/urls.py
 from django.urls import path
 from . import views
 urlpatterns = [
     path(r'', views.post_list, name='post_list'),
+    path(r'post/<int:pk>/', views.post_detail, name='post_detail'),
 ]
