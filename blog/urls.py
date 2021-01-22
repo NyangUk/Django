67103332e7a99528078af4c92b6cabe1ebebc246
Nyanguk <lobgd9150@gmail.com>
2021-01-22 23:02:49 +0900

    삭제하기 기능 구현

--- blog/urls.py
+++ blog/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     path(r'post/new', views.post_new, name='post_new'),
     path(r'^drafts/$', views.post_draft_list, name='post_draft_list'),
     path(r'^post/(?P<pk>\d+)/publish/$', views.post_publish, name='post_publish'),
+    path(r'^post/(?P<pk>\d+)/remove/$', views.post_remove, name='post_remove'),
 ]
