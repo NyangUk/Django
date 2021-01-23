9db167fa8a220aa0cd62de6b3499f80d10ace79e
Nyanguk <lobgd9150@gmail.com>
2021-01-23 23:06:17 +0900

    임시저장한 글 수정하기

--- blog/urls.py
+++ blog/urls.py
@@ -8,4 +8,5 @@ urlpatterns = [
     path(r'^drafts/$', views.post_draft_list, name='post_draft_list'),
     path(r'^post/(?P<pk>\d+)/publish/$', views.post_publish, name='post_publish'),
     path(r'^post/(?P<pk>\d+)/remove/$', views.post_remove, name='post_remove'),
+    path(r'^posst/(?P<pk>\d+)/edit/$', views.post_edit,name='post_edit')
 ]
--- blog/views.py
+++ blog/views.py
@@ -39,3 +39,17 @@ def post_remove(request,pk):
     post = get_object_or_404(Post, pk=pk)
     post.delete()
     return redirect('post_list')
+
+def post_edit(request,pk):
+    post = get_object_or_404(Post, pk=pk)   # 이전 게시글 받아오기
+    if request.method == "POST":        # 데이터를 저장해야할때
+        form = PostForm(request.POST)
+        if form.is_valid():
+            edit = form.save(commit=False)  # 바로저장 X
+            edit.author = request.user
+            # post.published_date = timezone.now()
+            post.save()                     # 바로저장 O
+            return redirect('post_detail', pk=post.pk)
+    else:                               # 입력 양식을 보여줘야 할때 
+        form = PostForm(instance = post)
+    return render(request, 'blog/post_edit.html', {'form': form})
