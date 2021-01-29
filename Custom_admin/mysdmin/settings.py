47738d5598b0c17f4d78d384f4e3b196be152016
Nyanguk <lobgd9150@gmail.com>
2021-01-29 23:36:43 +0900

    app추가 ,  AUTH_USER 모델 변경

--- Custom_admin/mysdmin/settings.py
+++ Custom_admin/mysdmin/settings.py
@@ -14,7 +14,7 @@ import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-
+AUTH_USER_MODEL = 'member.Member' # 'AppName.모델명'
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.0/howto/deployment/checklist/
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'member',
+    'post'
 ]
 
 MIDDLEWARE = [
