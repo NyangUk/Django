ac9db007c36d1307db3699d3ada9677d3160767d
Nyanguk <lobgd9150@gmail.com>
2021-01-29 23:37:15 +0900

    오타 및 오류사항 수정

--- Custom_admin/member/models.py
+++ Custom_admin/member/models.py
@@ -1,15 +1,14 @@
 from django.db import models
-
+from django.contrib.auth.models import AbstractUser
 # Create your models here.
-class Member(AbstractBaseUser):
+class Member(AbstractUser):
     TYPE_PERMISSIONS =  (
         ('AD','관리자'),
         ('ET','에디터'),
         ('MB','일반'),
     )
     email = models.EmailField('이메일', max_length=255, unique =True)
-    username = models.CharField('닉네임',max_length=30)
+    username = models.CharField('닉네임',max_length=30, unique =True)
     permission = models.CharField('권한', max_length=2, choices = TYPE_PERMISSIONS, default= 'MB')
-    certification_date = models.DateField('인증일', defualt=None, null =True, blank =True)
-    is_certificated =models.BooleanField('인증상태',defualt=False)
-    
+    certification_date = models.DateField('인증일', default=None, null =True, blank =True)
+    is_certificated =models.BooleanField('인증상태',default=False)
--- Custom_admin/post/models.py
+++ Custom_admin/post/models.py
@@ -1,19 +1,19 @@
-from django.db import models
-
+from django.db import models 
+from member.models import Member
 # Create your models here.
 class Category(models.Model):
-    name = models.models.CharField('카테고리이름', max_length=20)
+    name = models.CharField('카테고리이름', max_length=20)
 
 class Post(models.Model):
-    member = models.ForeignKey(Member,verbose_name='작성자')
-    category = models.ForeignKey(Category,verbose_name='카테고리')
+    member = models.ForeignKey(Member,verbose_name='작성자', on_delete=models.CASCADE)
+    category = models.ForeignKey(Category,verbose_name='카테고리', on_delete=models.CASCADE)
     title = models.CharField('제목', max_length=255)
     content = models.TextField('내용')
     is_deleted = models.BooleanField('삭제된글', default=False)
     created_at = models.DateTimeField('작성일', auto_now_add=True) 
 
 class Comment(models.Model):
-    member = models.ForeignKey(Member, verbose_name='작성자')
-    post = models.ForeignKey(Post, verbose_name='원본글')
+    member = models.ForeignKey(Member, verbose_name='작성자', on_delete=models.CASCADE)
+    post = models.ForeignKey(Post, verbose_name='원본글', on_delete=models.CASCADE)
     content = models.TextField()
     is_blocked = models.BooleanField('노출제한', default=False)
