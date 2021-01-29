f198cdb7b29f5c7d67af56b8f8ae8cea538a5288
Nyanguk <lobgd9150@gmail.com>
2021-01-29 20:57:55 +0900

    프로젝트 초기설정

--- Custom_admin/mysdmin/settings.py
+++ Custom_admin/mysdmin/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'md^5x0s^%9h52&tan7wv5fri8z(p+s66e5v)j8@5kt#y8qq3yw'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', '.pythonanywhere.com']
 
 
 # Application definition
@@ -105,7 +105,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Asia/Seoul'
 
 USE_I18N = True
 
@@ -118,3 +118,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
