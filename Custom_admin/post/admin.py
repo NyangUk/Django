0781fa7a36525af8f1fbd188f60e7c923188e11f
Nyanguk <lobgd9150@gmail.com>
2021-01-31 21:16:04 +0900

    Post 입력폼 커스텀

--- Custom_admin/post/admin.py
+++ Custom_admin/post/admin.py
@@ -1,7 +1,30 @@
+# Custom_admin/post/admin.py
 from django.contrib import admin
 from post.models import Category, Post, Comment
-
+from member.models import Member
 # Register your models here.
-admin.site.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_per_page = 10
+    list_display = (
+    'id', 'title', 'member','is_deleted', 'created_at', )
+    list_editable = ('is_deleted', )
+    list_filter = (
+    'member__permission',
+    'category__name', 'is_deleted', )
+    # fields = ('member','category','title',)
+    fieldsets = (
+        ('기본정보', {
+            "fields": (('member','category')),
+        }),
+        ('제목 및 내용', {
+            "fields": ('title','content',),
+        }),
+        ('삭제', {
+            "fields": ('is_deleted',),
+        })
+
+    )
+    
+admin.site.register(Post,PostAdmin)
 admin.site.register(Category)
 admin.site.register(Comment)
