d349b764b298bb8f7fd07ecf947955db5c36e99b
Nyanguk <lobgd9150@gmail.com>
2021-02-08 23:43:24 +0900

    동아리: home url 연결

---
+++ Custom_admin/post/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from . import views
+urlpatterns = [
+    path(r'', views.home ,name ='home'),
+    # path(r'post', views.post_list, name ='post_list'),
+    # path(r'post<int:pk>/', views.post_detail, name = 'post_detail'),
+    
+]
--- Custom_admin/post/views.py
+++ Custom_admin/post/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, get_object_or_404
+from .models import Post ,Category
 # Create your views here.
+posts=Post.objects.all()
+categorys =Category.objects.all()
+def home(requset):
+    post_category = []
+    for category in categorys:
+        post_category.append(Post.objects.filter(category = category))
+    context = {
+        'post_category' : post_category,
+    }
+    return render(requset ,'post/base.html', context)
+# def post_list(request):
+#     return render(request,'post')
